d04b7feba2869f6eb2aeca6d9d378027cd0faf91
Ben Galde <bgalde.dev@gmail.com>
2021-02-24 21:23:24 -0800

    Added visuals

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # MAIN SCRIPT TO RUN THE ETL FOR THE CRAIGSLIST PROPERTY RENTALS
 import config
 import cl_parser
+import visualization
 import datetime as dt
 
 start_time = dt.datetime.now()
@@ -8,6 +9,10 @@ start_time = dt.datetime.now()
 print('#' * 40)
 print('Running Craigslist Rental ETL')
 print('#' * 40)
+
+############################
+#        EXTRACTION        #
+############################
 
 # Create the craiglist parser
 my_parser = cl_parser.Parser() 
@@ -23,6 +28,17 @@ urls.append(config.url_lisings_southsd)
 my_parser.scrape_cl(urls)
 my_parser.update_details()
 
+############################
+#         VISUALS          #
+############################
+
+# Create the visualizer
+my_visualizer = visualization.Visualizer() 
+
+# Run the visuals
+my_visualizer.create_visuals()
+
+# Show the execution time
 end_time = dt.datetime.now()
 total_execute_time = (end_time-start_time).total_seconds()
 print(f'Total time to execute: {total_execute_time} seconds')
