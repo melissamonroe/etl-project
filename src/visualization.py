eb3fa201d2288cac5b3d5eed114dd788b9a95f86
Ben Galde <bgalde.dev@gmail.com>
2021-02-24 20:33:07 -0800

    Moved the assets to test

--- src/visualization.py
+++ src/visualization.py
@@ -7,6 +7,7 @@ import random
 import json
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 import config
 
@@ -98,8 +99,65 @@ class Visualizer:
 
         return df
 
+    def create_visuals(self):
+        # Generate the visualizations useful for the project
+        self.create_top20()
+
+    def get_summary_stats(self):
+        grouped_zip_df = self.get_raw_data().groupby(["listing_addrzip"])
+        # mean, median, variance, standard deviation, and SEM of the listing_price 
+        mean_listing_price = grouped_zip_df["listing_price"].mean()
+        zip_count = grouped_zip_df["listing_addrzip"].count()
+
+        # Assemble the resulting series into a single summary dataframe.
+        summary_stats_df = pd.DataFrame(
+            {"Average List Price": mean_listing_price,
+            "Listing Count": zip_count,
+            "Location": grouped_zip_df["listing_addrlocality"].unique(),
+            "Zip Code": grouped_zip_df["listing_addrzip"].unique(),
+            "Location (Zip)": grouped_zip_df["listing_addrlocality"].unique() + " " + grouped_zip_df["listing_addrzip"].unique()
+            })
+        return summary_stats_df
+
+    def create_top20(self):
+        summary_stats_df = self.get_summary_stats()
+        summary_stats_highpricezips_df = summary_stats_df.sort_values(by=['Average List Price'], ascending=False).head(20)
+
+        mean_highprices = []
+        locations_highprices = []
+        zipcodes_highprices = []
+
+        for m in summary_stats_highpricezips_df['Average List Price']:
+            mean_highprices.append(m)
+        for l in summary_stats_highpricezips_df['Location (Zip)']:
+            locations_highprices.append(l)        
+        for z in summary_stats_highpricezips_df['Zip Code']:
+            zipcodes_highprices.append(z)  
+        
+        x_axis = np.arange(0, len(locations_highprices))
+        tick_locations = []
+        for x in x_axis:
+            tick_locations.append(x)
+            
+        fig, ax = plt.subplots(figsize=(12,8)) # set the size that you'd like 
+        fig.subplots_adjust(bottom=0.4)
+        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                    ax.get_xticklabels() + ax.get_yticklabels()):
+            item.set_fontsize(14)
+        fig.suptitle('Top 20 Highest Average Rental Listing Price', fontsize=20)
+
+        plt.xlabel("Zip Code")
+        plt.ylabel("Average Rental Listing Price ($)")
+
+        plt.xlim(-0.75, len(locations_highprices)-.25)
+        plt.ylim(0, max(mean_highprices) + 1000)
+
+        plt.bar(x_axis,mean_highprices, facecolor="#097392", alpha=0.75, align="center")
+        plt.xticks(tick_locations, locations_highprices, rotation=90)
+        plt.savefig("../resources/images/fig01_top20averageprice.png")
         
 """ 
+# FOR TESTING
 visuals = Visualizer()
 base_df = visuals.get_clean_data()
 #print(base_df.head(5))
